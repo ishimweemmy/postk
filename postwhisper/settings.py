f1d93b07ecb4ad73579484722b7e557cfa2ce29b
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2023-05-23 23:17:26 +0200

    [fix]: removed a typo from settings

--- postwhisper/settings.py
+++ postwhisper/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = '*'
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -148,7 +147,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
+    'http://localhost:3000/',
+    'http://127.0.0.1:3000/',
     'https://postwhisper-frontend.vercel.app'
 ]
